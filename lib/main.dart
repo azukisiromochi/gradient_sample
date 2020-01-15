3acd2cd9406612273b1f1cf36e8b21fc72e0b4c8
@azukisiromochi <azukisiromochi@gmail.com>
2020-01-15 15:08:47 +0900

    Update: AppBar をグラデーション 🎨

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gradient_app_bar/gradient_app_bar.dart';
 
 void main() => runApp(MyApp());
 
@@ -66,10 +67,12 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
+      appBar: GradientAppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        backgroundColorStart: const Color(0xffe4a972),
+        backgroundColorEnd: const Color(0xff9941d8),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
